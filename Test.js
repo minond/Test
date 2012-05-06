4893a1adfcee49aef9cb9dfe7b5f7bb0fb3d8400
Marcos Minond <minond.marcos@gmail.com>
2012-05-05 21:33:06 -0600

    basic tests

--- Test.js
+++ Test.js
@@ -21,3 +21,114 @@
 
 "use strict";
 
+
+(function (global) {
+	/**
+	 * @constructor TestCase
+	 * @param mixed exact test result value
+	 */
+	var TestCase = function (expects) {
+		this.expects = expects;
+	};
+
+	/**
+	 * @name on
+	 * @param list of test case parameters
+	 */
+	TestCase.prototype.on = function () {
+		this.parameters = Array.prototype.splice.call(arguments, 0);
+	};
+
+	/**
+	 * @constructor TestResult
+	 * @param bool passed test case
+	 * @param mixed test result
+	 * @param mixed extected test result
+	 * @param array of test case parameters
+	 * @param int test case name/number
+	 */
+	var TestResult = function (pass, actual_grade, expected_grade, parameters, case_name) {
+		this.results = {
+			pass: !! pass,
+			expected: expected_grade,
+			actual: actual_grade,
+			case_name: test,
+			parameters, parameters
+		};
+	};
+
+	/**
+	 * @constructor Test
+	 * @param string test name
+	 */
+	var Test = window.Test = function (test_name) {
+		this.test_name = test_name;
+	};
+
+	/**
+	 * @name test_cases
+	 * array of test cases
+	 */
+	Test.prototype.cases = [];
+
+	/**
+	 * @name test_results
+	 * array of the test results
+	 */
+	Test.prototype.retultss = [];
+
+	/**
+	 * @name test
+	 * @param test function
+	 */
+	Test.prototype.test = function (test) {
+		this.test = test;
+	};
+
+	/**
+	 * @name expect
+	 * @param result value
+	 * @return TestCase instance
+	 */
+	Test.prototype.expect = function (result) {
+		this.cases.push(
+			new TestCase(result);
+		);
+
+		return this.cases[ this.cases.length - 1 ];
+	};
+
+	/**
+	 * @name run
+	 * @return void
+	 */
+	Test.prototype.run = function () {
+		var test, grade, pass;
+
+		// loop through all test cases
+		for (var i = 0, max = this.cases.length; i < max; i++) {
+			test = this.cases[ i ];
+			grade = this.test.apply(window, test.parameters);
+			pass = test.expects === grade;
+
+			this.results.push(
+				new TestResult(pass, grade, test.expects, test.parameters, i)
+			);
+		};
+	};
+})(Test);
+
+
+var eq_check = new Test;
+
+// the test
+eq_check.test(function (a, b) {
+	return a === b;
+});
+
+// the test cases
+eq_check.expect(true).on(1, 1);
+eq_check.expect(false).on(1, "1");
+
+// run the tests
+eq_check.run();
