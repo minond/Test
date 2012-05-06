c3de1369475257a4155264d373ddf1264513c82c
Marcos Minond <minond.marcos@gmail.com>
2012-05-06 00:23:38 -0600

    completed basic test manager/helper

--- Test.js
+++ Test.js
@@ -26,60 +26,46 @@
 	/**
 	 * @constructor TestCase
 	 * @param mixed exact test result value
+	 * 
+	 * creates a new test case setting the expected
+	 * test return value and defaulting the test case
+	 * function call parameters to an empty array - 
+	 * or no parameters.
 	 */
 	var TestCase = function (expects) {
 		this.expects = expects;
+		this.parameters = [];
 	};
 
 	/**
 	 * @name on
 	 * @param list of test case parameters
+	 * @return void
+	 * 
+	 * updates the parameter(s) for the test case call.
 	 */
 	TestCase.prototype.on = function () {
 		this.parameters = Array.prototype.splice.call(arguments, 0);
 	};
 
 	/**
-	 * @constructor TestResult
-	 * @param bool passed test case
-	 * @param mixed test result
-	 * @param mixed extected test result
-	 * @param array of test case parameters
-	 * @param int test case name/number
-	 */
-	var TestResult = function (pass, actual_grade, expected_grade, parameters, case_name) {
-		this.results = {
-			pass: !! pass,
-			expected: expected_grade,
-			actual: actual_grade,
-			case_name: test,
-			parameters, parameters
-		};
-	};
-
-	/**
 	 * @constructor Test
 	 * @param string test name
+	 * 
+	 * creates a new Test instance
 	 */
 	var Test = window.Test = function (test_name) {
 		this.test_name = test_name;
+		this.cases = [];
+		this.results = [];
 	};
-
-	/**
-	 * @name test_cases
-	 * array of test cases
-	 */
-	Test.prototype.cases = [];
-
-	/**
-	 * @name test_results
-	 * array of the test results
-	 */
-	Test.prototype.retultss = [];
 
 	/**
 	 * @name test
 	 * @param test function
+	 * @return void
+	 * 
+	 * set the Test's test function 
 	 */
 	Test.prototype.test = function (test) {
 		this.test = test;
@@ -89,10 +75,14 @@
 	 * @name expect
 	 * @param result value
 	 * @return TestCase instance
+	 * 
+	 * creates a new test case and returns the new 
+	 * test case's instance making updating the 
+	 * parameters an option.
 	 */
 	Test.prototype.expect = function (result) {
 		this.cases.push(
-			new TestCase(result);
+			new TestCase(result)
 		);
 
 		return this.cases[ this.cases.length - 1 ];
@@ -101,30 +91,151 @@
 	/**
 	 * @name run
 	 * @return void
+	 * 
+	 * loops though all test cases and saves
+	 * result data for each of the cases.
 	 */
-	Test.prototype.run = function () {
+	Test.prototype.run = function (times) {
 		var test, grade, pass;
 
+		if (this.results.length) {
+			return false;
+		}
+
+		if (!times) {
+			times = 1;
+		}
+
 		// loop through all test cases
-		for (var i = 0, max = this.cases.length; i < max; i++) {
-			test = this.cases[ i ];
-			grade = this.test.apply(window, test.parameters);
-			pass = test.expects === grade;
+		for (var j = 0; j < times; j++) {
+			for (var i = 0, max = this.cases.length; i < max; i++) {
+				test = this.cases[ i ];
+				grade = this.test.apply(window, test.parameters);
+				pass = test.expects === grade;
 
-			this.results.push(
-				new TestResult(pass, grade, test.expects, test.parameters, i)
-			);
-		};
+				// and save the test and result data
+				this.results.push({
+					pass: pass,
+					test_case: i,
+					actual: grade,
+					expected: test.expects,
+					parameters: test.parameters
+				});
+			}
+		}
 	};
-})(Test);
+
+	/**
+	 * @name run
+	 * @param string test_name
+	 * @param function test_function
+	 * @param array of test cases
+	 * @param int run count
+	 * @return Test instance
+	 * 
+	 * creates a new test and runs it.
+	 */
+	Test.run = function (test_name, test_function, test_cases, run_times) {
+		var test_case, test;
+		
+		// create the test
+		test = new Test(test_name);
+
+		// set the test function
+		test.test(test_function);
+		
+		// set all test cases
+		for (var i = 0, max = test_cases.length; i < max; i++) {
+			// set the expected return value
+			test_case = test.expect(test_cases[i][0]);
+
+			// set the parameters (if any)
+			if (test_cases[i].length === 2) {
+				test_case.on.apply(test_case, test_cases[i][1]);
+			}
+		}
+
+		// finally, run the test
+		test.run(run_times || 1);
+
+		return test;
+	};
+
+	/**
+	 * @name output
+	 * @param Test
+	 * @return bool output success
+	 * 
+	 * outputs test results to the console.
+	 */
+	Test.output = function (test) {
+		var ctest, out;
+
+		if (!test.results.length) {
+			return false;
+		}
+
+		console.log("------------------------------------------------------");
+		console.log("Test:", test.test_name);
+
+		for (var i = 0, max = test.results.length; i < max; i++) {
+			ctest = test.results[i];
+
+			out = (ctest.pass ? console.log : console.warn).bind(console);
+
+			out("Case #" + ctest.test_case, "-", ctest.pass ? "pass" : "fail");
+
+			if (!ctest.pass) {
+				out("Actual:", ctest.actual);
+				out("Extected:", ctest.expected);
+				out("Parameters:", ctest.parameters);
+			}
+		}
+
+		console.log("------------------------------------------------------");
+		console.log("");
+		console.log("");
+
+		return true;
+	};
+
+	/**
+	 * @name value
+	 * test short-cut functions
+	 */
+	Test.value = {
+		eq: function (a, b) {
+			return a === b;
+		},
+
+		ne: function (a, b) {
+			return a !== b;
+		},
+
+		lt: function (a, b) {
+			return a < b;
+		},
+
+		gt: function (a, b) {
+			return a > b;
+		},
+
+		le: function (a, b) {
+			return a <= b;
+		},
+
+		ge: function (a, b) {
+			return a >= b;
+		}
+	};
+})();
 
 
-var eq_check = new Test;
+
+var eq_check = new Test("Equality check no. 1");
 
 // the test
-eq_check.test(function (a, b) {
-	return a === b;
-});
+eq_check.test(Test.value.eq);
 
 // the test cases
 eq_check.expect(true).on(1, 1);
@@ -132,3 +243,11 @@ eq_check.expect(false).on(1, "1");
 
 // run the tests
 eq_check.run();
+
+
+var lt_check = Test.run("Less-than check no. 1", Test.value.lt, [
+	[true, [1, 10]]
+]);
+
+Test.output(eq_check);
+Test.output(lt_check);
